c0d38a0b76183ae20b5008975475e55ab77e278a
guolei <125311899@qq.com>
2020-04-29 15:35:12 +0800

    first changed

--- cloud-consumer/src/main/java/com/my/feign/UserFeignClient.java
+++ cloud-consumer/src/main/java/com/my/feign/UserFeignClient.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 //测试修改
-/* @FeignClient(name = "cloud-provider", fallbackFactory = FeignClientFallbackFactory.class) */
-@FeignClient(name = "cloud-provider")
+@FeignClient(name = "cloud-provider", fallbackFactory = FeignClientFallbackFactory.class)
+//@FeignClient(name = "cloud-provider")
 public interface UserFeignClient {
     @RequestMapping(value = "/findUser/{id}", method = RequestMethod.GET)
     public User findById(@PathVariable("id") Long id);
@@ -18,19 +18,19 @@ public interface UserFeignClient {
 /**
  * UserFeignClient的fallbackFactory类，该类需实现FallbackFactory接口，并覆写create方法
  */
-//@Component
-//class FeignClientFallbackFactory implements FallbackFactory<UserFeignClient> {
-//
-//    @Override
-//    public UserFeignClient create(Throwable cause) {
-//        return new UserFeignClient() {
-//            @Override
-//            public User findById(Long id) {
-//                User user = new User();
-//                user.setId("1");
-//                user.setName("默认用户");
-//                return user;
-//            }
-//        };
-//    }
-//}
+@Component
+class FeignClientFallbackFactory implements FallbackFactory<UserFeignClient> {
+
+    @Override
+    public UserFeignClient create(Throwable cause) {
+        return new UserFeignClient() {
+            @Override
+            public User findById(Long id) {
+                User user = new User();
+                user.setId("1");
+                user.setName("进入断路器");
+                return user;
+            }
+        };
+    }
+}
--- cloud-provider/src/main/java/com/my/controller/UserController.java
+++ cloud-provider/src/main/java/com/my/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.my.controller;
 
 import com.my.dao.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+//    @Value("${name}")
+//    private String name;
+
     @GetMapping("/findUser/{id}")
     public User findById(@PathVariable Long id) {
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         System.out.println("--------------start1");
         User findOne = new User();
         findOne.setName("hello zhangsan"+id);
@@ -16,4 +26,10 @@ public class UserController {
         System.out.println("--------------end");
         return findOne;
     }
+
+//    @RequestMapping("/test-config")
+//    public String getConfigName() {
+//        System.out.println("--------------getName:"+name);
+//        return name;
+//    }
 }
